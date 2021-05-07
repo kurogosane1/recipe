52a18819c7e6e88b82d7b4d193c82551f984565d
Syed Khurshid <magnite@hotmail.com>
2021-05-07 00:27:23 -0500

    Took the first step

---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background: green;
+  display: flex;
+`;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav } from "./Navbar.elements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+          <h2>This is the navbar</h2>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
